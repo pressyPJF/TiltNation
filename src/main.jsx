8b7750e79563f8d8ed8d42e1cec0781126cf59c9
pressyPJF <leleuthomas67@gmail.com>
2025-04-01 13:40:04 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TiltNation from './TiltNation'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TiltNation />
-  </React.StrictMode>
-)
