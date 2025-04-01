afbeb9950424ca8ae24139ad98759e42eb953884
pressyPJF <leleuthomas67@gmail.com>
2025-04-01 13:37:50 +0200

    Add files via upload

---
+++ src/TiltNation.jsx
@@ -0,0 +1,21 @@
+
+import { useState } from "react";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent
+} from "./ui/tabs";
+
+// Component content should be here (shortened for demonstration)
+export default function TiltNation() {
+  return (
+    <div className="p-4 space-y-4">
+      <h1 className="text-2xl font-bold text-center">Tilt Nation</h1>
+      {/* Rest of the component */}
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TiltNation from './TiltNation'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TiltNation />
+  </React.StrictMode>
+)
